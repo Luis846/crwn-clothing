39d3eb5afe322d417916a65993f8346c5ceb6162
Luis <luis.rivera846@gmail.com>
2020-02-11 21:50:39 -0500

    added redux and userreducer/useraction to header and app components

--- src/App.js
+++ src/App.js
@@ -6,36 +6,33 @@ import ShopPage from'./pages/shop/shop.components.jsx';
 import Header from './components/header/header-component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import{connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions'
 
 
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
+  
 //this handles how to close the application auth changes on firebase
   unsubscribeFromAuth = null;
 
 
 componentDidMount() {
+
+  const {setCurrentUser} = this.props;
   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if(userAuth){
       const userRef = await createUserProfileDocument(userAuth);
 
       userRef.onSnapshot(snapShot => {
-        this.setState({
-          currentUser: {
+        setCurrentUser ({
             id: snapShot.id,
           ...snapShot.data()
-          }
-        })
+          });
       });
     }
     else{
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     }
   })
 }
@@ -60,4 +57,8 @@ componentWillUnmount(){
 
 }
 
-export default App;
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+export default connect(null,mapDispatchToProps)(App);
