58b94e6e36d2e5598ee7dba728816bc312197d58
Luis <luis.rivera846@gmail.com>
2020-02-11 22:05:41 -0500

    updating app to redirect home if user is signed in

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,7 @@
+import {UserActionTypes} from './user.types';
+
 export const setCurrentUser = user =>({
     //always align creator type with reducer type expectation
-    type: 'SET_CURRENT_USER', //type always a string capital and snake case
+    type: UserActionTypes.SET_CURRENT_USER, //type always a string capital and snake case
     payload: user
 })
